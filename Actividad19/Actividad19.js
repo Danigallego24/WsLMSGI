108e21b6059985984e1a64ccb5dae28761754ceb
DaniGallego24_ <danigallegoarense@gmail.com>
2024-04-23 19:05:27 +0200

    Final actividad

--- Actividad19/Actividad19.js
+++ Actividad19/Actividad19.js
@@ -6,10 +6,10 @@ function mostrarContadorPares() {
     let numeroMaximo = parseInt(document.getElementById('numeroMax').value);
     let arrayPares = [];
     let contador = 0;
-    
-    for(let i = numeroMinimo; i <= numeroMaximo; i++) {
 
-        if(i % 2 == 0){
+    for (let i = numeroMinimo; i <= numeroMaximo; i++) {
+
+        if (i % 2 == 0) {
 
             arrayPares[contador] = i;
             contador++;
@@ -26,14 +26,123 @@ function mostrarContadorPares() {
 function conversorTemperaturaCentigrados() {
 
     let temperaturaCentigrados = parseInt(document.getElementById('temperaturaCentigrados').value);
-    let temperaturaFahrenheit = (temperaturaCentigrados * 9/5) + 32
+    let temperaturaFahrenheit = (temperaturaCentigrados * 9 / 5) + 32
 
     document.getElementById('temperaturaFahrenheit').value = temperaturaFahrenheit;
 }
 function conversorTemperaturaFahrenheit() {
 
     let tempFahrenheit = parseInt(document.getElementById('tempFahrenheit').value);
-    let tempCentigrados = (tempFahrenheit - 32) * 5/9;
+    let tempCentigrados = (tempFahrenheit - 32) * 5 / 9;
 
     document.getElementById('tempCentigrados').value = tempCentigrados;
+}
+function adivinarNumero() {
+
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    let intentoNumero;
+    let contadorIntentos = 0;
+
+    while (intentoNumero !== randomNumber) {
+
+        intentoNumero = parseInt(prompt("Adivina el número entre 1 y 100:"));
+
+        if (intentoNumero < randomNumber) {
+
+            alert("Muy bajo. Intenta de nuevo.");
+
+        } else if (intentoNumero > randomNumber) {
+
+            alert("Muy alto. Intenta de nuevo.");
+
+        } else {
+
+            alert("ACERTASTE ENHORABUENA");
+
+        }
+
+        contadorIntentos++;
+
+    }
+
+}
+
+function cambiarColorPag() {
+
+    let colorPag = document.getElementById('colorPag').value;
+
+    document.body.style.backgroundColor = colorPag;
+
+}
+
+function introducirContrasenyaUsuario() {
+
+    let usuario = document.getElementById('usuario').value;
+    let contrasenya = document.getElementById('contrasenya').value;
+
+    if (usuario === "goku" && contrasenya === "Vegeta777") {
+
+        alert("Bienvenute un pikito??");
+
+    } else {
+
+        alert("Ohhhhh fallaste rey");
+
+    }
+
+}
+
+function verificarSiEsPrimo() {
+
+    let numero = parseInt(document.getElementById('numero').value);
+    let esPrimo = true;
+
+    if (numero <= 1) {
+
+        esPrimo = false;
+
+    } else {
+
+        for (let i = 2; i <= Math.sqrt(numero); i++) {
+
+            if (numero % i === 0) {
+
+                esPrimo = false;
+
+                break;
+
+            }
+
+        }
+
+    }
+
+    if (esPrimo) {
+
+        alert(numero + ' EL PRIMOOOOO!!!!!.');
+
+    } else {
+
+        alert(numero + ' no es un número primo.');
+
+    }
+
+}
+
+function generateFibonacci() {
+
+    let numeroN = parseInt(document.getElementById('fibonacci').value);
+    let fibonacci = [0, 1];
+
+    for (let i = 2; i < numeroN; i++) {
+
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+
+    }
+
+    for (let i = 0; i < numeroN; i++) {
+
+        console.log(fibonacci[i]);
+
+    }
 }
